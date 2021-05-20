3654e8160dc8740c500ea7ed88264fc27761db55
Matic <matic.drobez@gmail.com>
2021-05-20 12:02:23 +0200

    Handling errors in Rust.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() {
     let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            //return Err(e);
+            eprintln!("Oh here is error: {}", e.to_string());
+            std::process::exit(1);
+        }
     }
     if !silent {
         eprintln!("Total bytes: {}", total_bytes);
